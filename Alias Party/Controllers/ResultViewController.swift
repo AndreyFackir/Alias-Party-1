6a7abdcb0dcf24e6ec11089e86fc7af4c5e9093c
AndreyFackir <104596263+AndreyFackir@users.noreply.github.com>
2022-05-11 19:25:03 +0300

    some

--- Alias Party/Controllers/ResultViewController.swift
+++ Alias Party/Controllers/ResultViewController.swift
@@ -9,28 +9,21 @@ import UIKit
 import AVFoundation
 
 class ResultViewController: UIViewController {
-    var some = 0
     
     var soundManager = SoundManager()
-    
     var jokeManager = JokeManager()
-    
     var teamOneScoreInt = 0
     var teamTwoSCoreInt = 0
-
-   @IBOutlet var teamOneScore: UILabel!
-    @IBOutlet var teamTwoScore: UILabel!
     
+    @IBOutlet var teamOneScore: UILabel!
+    @IBOutlet var teamTwoScore: UILabel!
     @IBOutlet var jokeLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         teamOneScore.text = String(teamOneScoreInt)
         teamTwoScore.text = String(teamTwoSCoreInt)
-        
-
         setupViews()
-        
     }
     
     private func setupViews() {
@@ -42,20 +35,16 @@ class ResultViewController: UIViewController {
     
     @IBAction func continueButtonPressed(_ sender: UIButton) {
         soundManager.playSound(soundName: "button")
-        
     }
     
     @IBAction func newGameButtonPressed(_ sender: UIButton) {
         soundManager.playSound(soundName: "button")
-        
     }
     
     @IBAction func goMainViewButtonPressed(_ sender: Any) {
         navigationController?.popToRootViewController(animated: true)
         soundManager.playSound(soundName: "button")
     }
-    
-    
 }
 //MARK: - JOKE Manager
 
@@ -63,7 +52,7 @@ extension ResultViewController: JokeManagerDelegate {
     func didUpdateJoke(joke: JokeModel, punchline: JokeModel) {
         
         DispatchQueue.main.async {
-        
+            
             self.jokeLabel.text = joke.setup
             Timer.scheduledTimer(withTimeInterval: 1, repeats: false) { timer in
                 
